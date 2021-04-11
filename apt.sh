a7995d3c94914b318f49bfbb755974bc6947a88f
TamaGoes0711 <tamatama@tensi.org>
2021-04-11 21:54:05 +0900

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 function inst(){
-  echo -en '\e[1;37;49mInstalling '$1'...\e[0m'
+  echo -e '\e[1;37;49mInstalling '$1'...\e[0m'
 ##  echo -e '\e[0;37;49m'
   sudo apt -y install $1
   echo -e '\e[1;37;49mInstalled '$1'\e[0m'
   echo -en '\e[1;37;49mChecking command '$1'...\e[0m'
-  echo -e '\e[2;37;49m'
+##  echo -e '\e[2;37;49m'
   if [ -n $(which $1) ]; then
     echo "You can use $1"
   else
@@ -15,12 +15,12 @@ function inst(){
   echo
 }
 
-echo -en '\e[1;37;49mUpdating...\e[0m'
+echo -e '\e[1;37;49mUpdating...\e[0m'
 ## echo -e '\e[0;30;49m'
 sudo apt update -y
 echo -e '\e[1;37;49mUpdated\e[0m'
 echo
-echo -en '\e[1;37;49mUpgrading...\e[0m'
+echo -e '\e[1;37;49mUpgrading...\e[0m'
 ## echo -e '\e[2;37;49m'
 sudo apt upgrade -y
 echo -e '\e[1;37;49mUpgraded\e[0m'
@@ -34,12 +34,12 @@ done
 
 rm -rf installlist.txt
 
-echo -en '\e[1;37;49mInstalling Pi-Apps...\e[0m'
+echo -e '\e[1;37;49mInstalling Pi-Apps...\e[0m'
 ## echo -e '\e[2;37;49m'
 wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
 echo -e '\e[1;37;49mInstalled\e[0m'
 
-echo -en '\e[1;37;49mInstalling Node.js...\e[0m'
+echo -e '\e[1;37;49mInstalling Node.js...\e[0m'
 ## echo -e '\e[2;37;49m'
 curl -L https://git.io/v7kPb | bash
 echo -e '\e[1;37;49mInstalled\e[0m'

01dfa829d12f50b0cfad0183066b1528788e92db
TamaGoes0711 <tamatama@tensi.org>
2021-04-11 13:48:24 +0100

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -2,7 +2,7 @@
 
 function inst(){
   echo -en '\e[1;37;49mInstalling '$1'...\e[0m'
-  echo -e '\e[0;37;49m'
+##  echo -e '\e[0;37;49m'
   sudo apt -y install $1
   echo -e '\e[1;37;49mInstalled '$1'\e[0m'
   echo -en '\e[1;37;49mChecking command '$1'...\e[0m'
@@ -16,12 +16,12 @@ function inst(){
 }
 
 echo -en '\e[1;37;49mUpdating...\e[0m'
-echo -e '\e[0;30;49m'
+## echo -e '\e[0;30;49m'
 sudo apt update -y
 echo -e '\e[1;37;49mUpdated\e[0m'
 echo
 echo -en '\e[1;37;49mUpgrading...\e[0m'
-echo -e '\e[2;37;49m'
+## echo -e '\e[2;37;49m'
 sudo apt upgrade -y
 echo -e '\e[1;37;49mUpgraded\e[0m'
 
@@ -35,12 +35,12 @@ done
 rm -rf installlist.txt
 
 echo -en '\e[1;37;49mInstalling Pi-Apps...\e[0m'
-echo -e '\e[2;37;49m'
+## echo -e '\e[2;37;49m'
 wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
 echo -e '\e[1;37;49mInstalled\e[0m'
 
 echo -en '\e[1;37;49mInstalling Node.js...\e[0m'
-echo -e '\e[2;37;49m'
+## echo -e '\e[2;37;49m'
 curl -L https://git.io/v7kPb | bash
 echo -e '\e[1;37;49mInstalled\e[0m'
 

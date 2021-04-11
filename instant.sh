818a597145812ce7b33c6b2c4e7f1f157e43d5a0
TamaGoes0711 <tamatama@tensi.org>
2021-04-11 13:39:44 +0100

    Update instant.sh

--- instant.sh
+++ instant.sh
@@ -4,5 +4,12 @@ function start(){
     wget -qO- https://raw.githubusercontent.com/TamaTamaGoGo/raspi-instant-setup/main/$1 | bash
 }
 
+mkdir raspi-instant-setup
+cd raspi-instant-setup
+
+rm -rf colortest.sh
+rm -rf apt.sh
+rm -rf installlist.txt
+
 start colortest.sh
 start apt.sh

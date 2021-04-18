5bbd9f1767cfb022c24eb1b6678cd48211854453
TamaGoes0711 <tamatama@tensi.org>
2021-04-18 08:08:13 +0100

    Update instant.sh

--- instant.sh
+++ instant.sh
@@ -4,7 +4,7 @@ function start(){
     wget -qO- https://raw.githubusercontent.com/TamaTamaGoGo/raspi-instant-setup/main/$1 | bash
 }
 
-echo 'To prevent berryboot's issue, we change swap to off'
+echo 'To prevent berryboots issue, we change swap to off'
 sudo systemctl disable dphys-swapfile
 
 mkdir raspi-instant-setup
@@ -16,6 +16,13 @@ rm -rf installlist.txt
 
 start colortest.sh
 start apt.sh
+
+wget https://cli-assets.heroku.com/branches/stable/heroku-linux-arm.tar.gz
+mkdir -p /usr/local/lib /usr/local/bin
+sudo tar -xvzf heroku-linux-arm.tar.gz -C /usr/local/lib
+sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
+heroku -v
+rm -rf heroku-linux-arm.tar.gz
 
 echo 'If you not using berryboot and you want to run swap, please run'
 echo 'sudo systemctl enable dphys-swapfile'
